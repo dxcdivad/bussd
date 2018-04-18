a60ebc2701966131936870ece38c5af6557c4f39
Gareth Bk <garethbk@gmail.com>
2018-04-18 16:45:43 -0700

    Jest env

--- server/app.js
+++ server/app.js
@@ -15,13 +15,6 @@ app.use(bodyParser.json());
 app.use(morgan('dev'));
 app.use(express.static('build'));
 
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
-
 // app.get('/', (req, res) => {
 //   res.status(200) ? console.log('Everything looks fine...') : console.log("There's an error...");
 //   res.json({
