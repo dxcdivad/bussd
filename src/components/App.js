3e3e0bab5bdf2f51b9aab3e4e305d7a5a1ce1ed7
Gareth Bk <garethbk@gmail.com>
2018-04-20 13:32:18 -0700

    Resolving merge conflicts

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import http from '../../node_modules/axios/lib/adapters/http';
+
 import '../App.css';
 
 // import '../App.css';
 import Map from './Map.js';
 import Styles from './Styles.js';
-import { H1,H2,AppHeader,AppBody,MapContainer,BodyContainer } from './Styles.js';
+import { H1, H2, AppHeader, AppBody, MapContainer, BodyContainer } from './Styles.js';
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      response: 'default'
+      response: 'default',
+      vehicles: ''
     };
+
+    this.getVehicleData = this.getVehicleData.bind(this);
   }
 
   componentDidMount() {
@@ -41,27 +45,37 @@ class App extends Component {
     );
   };
 
+  getVehicleData() {
+    const config = { adapter: http, headers: { 'Access-Control-Allow-Origin': '*' } };
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+    const remoteUrl =
+      'https://realtime.sdmts.com/api/api/gtfs_realtime/vehicle-positions-for-agency/MTS.pbtext?key=ac6279cf-c99b-4e09-8434-5ab4c019479c';
+
+    axios.get(proxyUrl + remoteUrl, config).then(res => {
+      const parsedRes = JSON.stringify(res.data);
+      //parsedRes = JSON.parse(parsedRes);
+      this.setState({ vehicles: parsedRes });
+    });
+  }
+
   render() {
-    
     return (
-      
       <div className="App">
-      
-       <AppHeader>
-         <H2><h1 className="App-title">Bussd</h1></H2>  
-       </AppHeader>
-       
-       <AppBody>
-         <BodyContainer >
-          <MapContainer>
-            <Map  />
-          </MapContainer>
-          </BodyContainer >
-       </AppBody>
-     
-     </div> 
-       
-       
+        <AppHeader>
+          <H2>
+            <h1 className="App-title">Bussd</h1>
+          </H2>
+        </AppHeader>
+
+        <AppBody>
+          <BodyContainer>
+            <MapContainer>
+              <Map />
+            </MapContainer>
+            <button onClick={this.getVehicleData}>Get Vehicle Data</button>
+          </BodyContainer>
+        </AppBody>
+      </div>
     );
   }
 }
