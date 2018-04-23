9ab876e4ee28082aeaea7057393e274fe125e876
Gareth Bk <garethbk@gmail.com>
2018-04-23 15:35:59 -0700

    Making map responsive

--- src/components/Styles.js
+++ src/components/Styles.js
@@ -1,60 +1,62 @@
 import styled from 'styled-components';
-import image from  '../images/bus1.jpg';
+import image from '../images/bus1.jpg';
 
 export const H1 = styled.h1`
-font-family: 'Advent Pro', 'Open Sans', sans-serif;
-margin-left: 2%px;
-margin-top:-3.5px;
+  font-family: 'Advent Pro', 'Open Sans', sans-serif;
+  margin-left: 2%px;
+  margin-top: -3.5px;
 `;
 export const H2 = styled.h2`
-font-family: 'Advent Pro', 'Open Sans', sans-serif;
-margin-left: 2%;
-margin-top: -2%;
+  font-family: 'Advent Pro', 'Open Sans', sans-serif;
+  margin-left: 2%;
+  margin-top: -2%;
 `;
 export const H3 = styled.h3`
-font-family: 'Advent Pro', 'Open Sans', sans-serif;
+  font-family: 'Advent Pro', 'Open Sans', sans-serif;
 `;
 export const H4 = styled.h4`
-font-family: 'Advent Pro', 'Open Sans', sans-serif;
+  font-family: 'Advent Pro', 'Open Sans', sans-serif;
 `;
 export const H5 = styled.h5`
-font-family: 'Advent Pro', 'Open Sans', sans-serif;
+  font-family: 'Advent Pro', 'Open Sans', sans-serif;
 `;
 export const H6 = styled.h6`
-font-family: 'Advent Pro', 'Open Sans', sans-serif;
-`; 
-export const AppHeader = styled.header`
-background-color: #222;
-height: 70px;
-padding-top:1px;
-color:white;
+  font-family: 'Advent Pro', 'Open Sans', sans-serif;
 `;
+export const AppHeader = styled.header`
+  background-color: #222;
+  color: white;
+  display: flex;
+  width: 100%;
+  height: 70px;
+`;
+
 export const AppBody = styled.div`
+ /*  background: linear-gradient(rgba(250, 250, 250, 0.7), rgba(250, 250, 250, 0.7)), url(${image});
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  background-repeat: no-repeat; */
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  width: 100%;
 
-padding-top:1px;
-background:
-linear-gradient(
-  rgba(250, 250, 250, 0.7),
-  rgba(250, 250, 250, 0.7)
-),
-url(${image});
-width:100%;
-height:100%;
-position: absolute; 
-background-repeat: no-repeat;
-`; 
+  > div {
+    width: 100% !important;
+  }
+`;
+
 export const MapContainer = styled.div`
- margin-left: 400px;
- margin-top: 40px;
- height:40%;
+  margin: 50px auto;
+  width: 100%;
+`;
 
-`; 
 export const BodyContainer = styled.div`
-margin-top: -2px;
+  /* margin-top: -2px;
 background-color:rgba(0, 0, 0, 0.7);
 position absolute;
 width:100%;
-height:87%;
-`; 
-
-
+height:87%; */
+`;
