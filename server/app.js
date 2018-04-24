247fff9887913782593cd9e6d01152ee5c5216eb
Gareth Bk <garethbk@gmail.com>
2018-04-24 09:52:00 -0700

    Working version ready to remote deploy

--- server/app.js
+++ server/app.js
@@ -9,6 +9,9 @@ const Trip = require('./models/Trip');
 const fs = require('fs');
 const csvjson = require('csvjson');
 const path = require('path');
+const dotenv = require('dotenv');
+dotenv.load();
+
 const cors = require('cors');
 
 mongoose.connect(process.env.REACT_APP_DB_LOCATION);
@@ -17,6 +20,7 @@ mongoose.Promise = Promise;
 
 const app = express();
 const router = express.Router();
+
 app.use(cors());
 
 app.use(bodyParser.json());
@@ -32,23 +36,20 @@ var options = {
   quote: '"' // optional
 };
 
-var routejson = csvjson.toSchemaObject(routeData, options);
-var stoptimesjson = csvjson.toSchemaObject(stopTimesData, options);
-var stopjson = csvjson.toSchemaObject(stopData, options);
-var tripsjson = csvjson.toSchemaObject(tripsData, options);
+var routejson = csvjson.toObject(routeData, options);
+var stoptimesjson = csvjson.toObject(stopTimesData, options);
+var stopjson = csvjson.toObject(stopData, options);
+var tripsjson = csvjson.toObject(tripsData, options);
 
-Route.collection.insert(routejson, function(err, result) {
+/* Route.collection.insert(routejson, function(err, result) {
   console.log(result);
 });
-
 Stop.collection.insert(stopjson, function(err, result) {
   console.log(result);
 });
-
 StopTime.collection.insert(stoptimesjson, function(err, result) {
   console.log(result);
 });
-
 Trip.collection.insert(tripsjson, function(err, result) {
   console.log(result);
 });*/
