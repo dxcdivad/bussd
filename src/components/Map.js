96b734605f01accf2cd99e89e80c99def7ab3ce7
Gareth Bk <garethbk@gmail.com>
2018-04-25 13:03:41 -0700

    Synching with dev branch

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,29 +1,26 @@
 import React, { Component } from 'react';
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
 import { geolocated } from 'react-geolocated';
-<<<<<<< HEAD
 import { LoadingContainer } from './Styles';
-=======
 import { GeoLocation } from 'react-geolocation';
-import { MarkerClusterer } from "react-google-maps/lib/components/addons/MarkerClusterer";
->>>>>>> bb9d9d324f32223d2a212c51c7491e56a8a8be76
+import { MarkerClusterer } from 'react-google-maps/lib/components/addons/MarkerClusterer';
 
 class Map extends Component {
   render() {
-
     const MapWithAMarker = withScriptjs(
-
       withGoogleMap(props => (
         <GoogleMap
-          options={{minZoom:15}}
+          options={{ minZoom: 15 }}
           defaultZoom={19}
           defaultCenter={{ lat: this.props.coords.latitude, lng: this.props.coords.longitude }}
-          ref={(ref) => { this.map = ref; }}
+          ref={ref => {
+            this.map = ref;
+          }}
           onIdle={props.onMapIdle}
         >
-        <Marker position={{ lat: this.props.coords.latitude, lng: this.props.coords.longitude }} />
-        {this.props.vehicles
-              ? this.props.vehicles.map(vehicle => {
+          <Marker position={{ lat: this.props.coords.latitude, lng: this.props.coords.longitude }} />
+          {this.props.vehicles
+            ? this.props.vehicles.map(vehicle => {
                 const icon = 'https://s3.us-east-2.amazonaws.com/garethbk-portfolio/bus-icon.png';
                 const iconRed = 'https://s3.us-east-2.amazonaws.com/garethbk-portfolio/bus-icon-red.png';
                 if (vehicle.location !== null && vehicle.tripStatus !== null) {
@@ -35,20 +32,13 @@ class Map extends Component {
                   return <Marker position={{ lat: vehicle.location.lat, lng: vehicle.location.lon }} icon={icon} />;
                 }
               })
-              : console.log('no vehicles')}
-          <MarkerClusterer
-            averageCenter
-            enableRetinaIcons
-            gridSize={60}
-          >
-            
-
-
+            : console.log('no vehicles')}
+          <MarkerClusterer averageCenter enableRetinaIcons gridSize={60}>
             {this.props.stops
               ? this.props.stops.map(stop => {
-                const stopIcon = 'https://s3.us-east-2.amazonaws.com/garethbk-portfolio/bus-stop-icon.png';
-                return <Marker position={{ lat: stop.stopLat, lng: stop.stopLon }} icon={stopIcon} />;
-              })
+                  const stopIcon = 'https://s3.us-east-2.amazonaws.com/garethbk-portfolio/bus-stop-icon.png';
+                  return <Marker position={{ lat: stop.stopLat, lng: stop.stopLon }} icon={stopIcon} />;
+                })
               : console.log('no stops')}
           </MarkerClusterer>
         </GoogleMap>
@@ -73,15 +63,10 @@ class Map extends Component {
         />
       </div>
     ) : (
-<<<<<<< HEAD
       <LoadingContainer>
         <h1>Loading...</h1>
       </LoadingContainer>
     );
-=======
-            <div>Getting the location data&hellip; </div>
-          );
->>>>>>> bb9d9d324f32223d2a212c51c7491e56a8a8be76
   }
 }
 export default geolocated({
