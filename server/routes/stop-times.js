563383a308ac23ffc5e033199dce8dcae5823c48
KeeganJustis <keeganjustis@gmail.com>
2018-04-27 13:56:40 -0700

    fixed buss stop

--- server/routes/stop-times.js
+++ server/routes/stop-times.js
@@ -3,20 +3,25 @@ const router = express.Router();
 const StopTime = require('../models/StopTime');
 ///not done
 
-
-
-
  router.get('/:id', (req, res) => {
   var id = req.params.id;
   StopTime.find(
     { 'stopId': id },
-    ' tripId stopId arrivalTime departureTime:',
+    ' tripId stopId arrivalTime departureTime',
     function(err, StopTime) {
       if (err) return handleError(err);
       console.log(StopTime.stopId);
       res.status(200).json(StopTime);
     }
   );
+
+//   StopTime.find({ stopId: id }, 'tripId stopId arrivalTime departureTime', function(err, StopTime) {
+//     if (err) return handleError(err);
+//     console.log(StopTime.stopId);
+//     res.status(200).json(StopTime);
+//   });
+// }); 
+
 });
 
 module.exports = router;
--- server/routes/vehicle.js
+++ server/routes/vehicle.js
@@ -1,28 +1,24 @@
 const express = require('express');
 const router = express.Router();
 // const cache = require('express-redis-cache')({ client: require('redis').createClient(),expire: 20 });
-var cache = require('express-redis-cache')( process.env.REACT_APP_REDIS_CONN
-    );
-const axios =require('axios');
-
-router.get('/', cache.route(),function(req,res) {
-    axios.get(process.env.REACT_APP_REDIS_API)
-    .then(function (response) {
-        res.status(200).json(response.data.data.list);
-        console.log(response.data.data.list);
-     })
-    .catch(function (error) {
-    console.log(error);
-  });
-
-    console.log('get the api');  
-    
-
-
-
+var cache = require('express-redis-cache')({
+  client: require('redis').createClient(process.env.REDIS_URL),
+  expire: 20
 });
+const axios = require('axios');
 
+router.get('/', cache.route(), function(req, res) {
+  axios
+    .get(process.env.REACT_APP_REDIS_API)
+    .then(function(response) {
+      res.status(200).json(response.data.data.list);
+      console.log(response.data.data.list);
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
 
-
+  console.log('get the api!');
+});
 
 module.exports = router;
